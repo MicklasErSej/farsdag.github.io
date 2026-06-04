60f24d2c64a41dbabda65ff51abb9270e0c90379
MicklasErSej <92669958+MicklasErSej@users.noreply.github.com>
2026-06-04 19:34:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -66,6 +66,22 @@ const questions = [
 {
     q: "How ready are you for today's mission?",
     a: ["Very ready", "Somewhat ready", "Not ready but excited"]
+},
+{
+    q: "How ready are you for today's mission?",
+    a: ["Very ready", "Somewhat ready", "Not ready but excited"]
+},
+{
+    q: "How ready are you for today's mission?",
+    a: ["Very ready", "Somewhat ready", "Not ready but excited"]
+},
+{
+    q: "How ready are you for today's mission?",
+    a: ["Very ready", "Somewhat ready", "Not ready but excited"]
+},
+{
+    q: "How ready are you for today's mission?",
+    a: ["Very ready", "Somewhat ready", "Not ready but excited"]
 }
 ];
 
